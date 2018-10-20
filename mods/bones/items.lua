4ee218501e9365ba74bd07c96a16d8d8f51edea1
Doloment <sawerter@inbox.ru>
2018-10-21 02:07:32 +0700

    Update items.lua

--- mods/bones/items.lua
+++ mods/bones/items.lua
@@ -3,8 +3,6 @@ local SL = lord.require_intllib()
 minetest.register_craftitem("bones:bonedust", {
 	description = SL("Bone Dust"),
 	inventory_image = "bones_bonedust.png",
-	liquids_pointable = false,
-	stack_max = 99,
 })
 
 minetest.register_craftitem("bones:bone", {
@@ -15,6 +13,7 @@ minetest.register_craftitem("bones:bone", {
 minetest.register_tool("bones:bone_scythe", {
 	description = SL("Bone Scythe"),
 	inventory_image = "bones_scythe.png",
+	wield_scale = {x=2,y=2,z=1},
 	tool_capabilities = {
 		full_punch_interval = 0.7,
 		max_drop_level=1,
